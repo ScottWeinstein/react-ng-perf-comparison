aa6b0bc868991497d1436c5255566cb8ec4f92a8
Joel Zipkin <yzipkin@gmail.com>
2017-06-09 16:26:58 -0400

    upgrade angular to v4.1.3 and changed `npm run build` to publish directly into the docs/ang folder (no more app specific build folder)

--- ang/src/app/app.component.ts
+++ ang/src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
-@Component({
-  selector: 'app-root',
-  template: `
-     Angular Tickets Instantiated: {{ initialQuantity }}
-      <group-view [initialQuantity]="initialQuantity"></group-view>
-  `,
-  changeDetection: ChangeDetectionStrategy.OnPush
-})
+@Component(
+  {
+    selector: 'app-root',
+    template: `
+		Source Repo: <a href="https://github.com/ScottWeinstein/react-ng-perf-comparison"> 
+        https://github.com/ScottWeinstein/react-ng-perf-comparison</a>
+		<br/>
+    Angular Tickets Instantiated: {{ initialQuantity }} | Update Speed: 50ms
+		<group-view [initialQuantity]="initialQuantity"></group-view>
+    `,
+    changeDetection: ChangeDetectionStrategy.OnPush
+  })
 export class AppComponent {
   initialQuantity: number = 500;
 }
